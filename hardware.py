8210b273e61c2bf99a453615ba32b0ae3191b146
feitang0 <iamtangfei@gmail.com>
2025-12-18 13:40:20 +0800

    Add T4 and TPU

--- hardware.py
+++ hardware.py
@@ -1,3 +1,10 @@
 HARDWARE = {
-            "nvidia_A800_80G_PCIe": {"bandwidth": 1935e9, "FP16": 312e12, "INT8": 624e12, "onchip_buffer": 27648e3},
-        }
+    "nvidia_A800_80G_PCIe": {
+        "bandwidth": 1935e9,
+        "FP16": 312e12,
+        "INT8": 624e12,
+        "onchip_buffer": 27648e3,
+    },
+    "nvidia_T4": {"bandwidth": 320e9, "FP16": 65e12},
+    "google_TPU_v5e1": {"bandwidth": 819e9, "FP16": 197e12},
+}
